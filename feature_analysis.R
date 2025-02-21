af213ea21f62c63b87a2cbf18c7bb022dff377f9
ErikMatheus <erik.leichtenberg@gmail.com>
2025-02-21 17:43:14 +0100

    HW version

---
+++ feature_analysis.R
@@ -0,0 +1,12 @@
+library(jsonlite)
+library(tidyverse)  # We'll need this for data manipulation and visualization
+
+# Read the JSON file
+features <- fromJSON("C:/Users/erikl/Downloads/compmus-corpus-2025/features/gijs-s-2.json")
+
+# Look at the structure
+str(features)
+
+
+
+
